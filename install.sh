8beca8ace054a5f817f796b26371e45d0f33c3a3
namasme <nacho.mas@datadoghq.com>
2025-02-03 11:42:16 +0000

    Initial version — installs brew, OrgStore CLI, & fzf

--- install.sh
+++ install.sh
@@ -25,3 +25,19 @@ while read df; do
   mkdir -p "$(dirname "$link")"
   ln -sf "$df" "$link"
 done
+
+# Update repositories
+sudo apt update && sudo apt upgrade
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && echo "export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH" >> ~/.zshenv
+## Add Datadog tap
+source .zshenv
+brew tap datadog/tap git@github.com:DataDog/homebrew-tap.git
+brew update
+## Install OrgStore CLI
+brew install orgstore
+
+# Install fzf
+sudo apt install fzf
