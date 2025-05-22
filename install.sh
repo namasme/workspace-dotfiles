0d48d348be92df3f8054aec3c6da217039d2661e
namasme <nacho.mas@datadoghq.com>
2025-05-22 13:47:00 +0000

    Adds stow to the mix to symlink dotfiles to their target location

--- install.sh
+++ install.sh
@@ -41,3 +41,15 @@ brew install orgstore
 
 # Install fzf
 sudo apt install fzf
+
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Handle dotfiles
+## Install stow
+sudo apt install stow
+## Symlink dotfiles
+cd dotfiles
+stow tmux
+stow zsh
+
