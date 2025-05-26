cf5187a6fddd78cee7dc0f004eacd4fbbdc8a150
Nacho Mas <nacho.mas@datadoghq.com>
2025-05-26 15:14:06 +0200

    Updates how brew ends up in the PATH
    * Instead of manually creating the .zshenv file it is now stowed.

---
+++ dotfiles/zsh/.zshenv
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Putting linuxbrew's bin at the beginning causes devtools/update-tool to fail
+# see https://github.com/DataDog/devtools/blob/ff5e510b48e00a07dbae0e495c490d03c8e93ef3/apps/update-tool/update_tool.py#L294-L319
+export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ sudo apt update && sudo apt upgrade
 
 # Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && echo 'export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ~/.zshenv
+## Add Homebrew to PATH for the remainder of the script
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
 ## Add Datadog tap
-source ~/.zshenv
 brew tap datadog/tap git@github.com:DataDog/homebrew-tap.git
 brew update
 ## Install fx
