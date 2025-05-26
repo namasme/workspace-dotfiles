fa7f6de34aca15034d1a48b0bd9e2d1d94244c26
Nacho Mas <nacho.mas@datadoghq.com>
2025-05-26 15:02:40 +0200

    Fixes .zshenv PATH prepend
    * Using double quotes was causing the reference to $PATH to be expanded

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo apt update && sudo apt upgrade
 
 # Install Homebrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && echo "export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH" >> ~/.zshenv
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && echo 'export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ~/.zshenv
 ## Add Datadog tap
 source ~/.zshenv
 brew tap datadog/tap git@github.com:DataDog/homebrew-tap.git
