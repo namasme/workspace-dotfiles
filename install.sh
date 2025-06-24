11f0cfe5495f3fede91ac15f365632115f91b0ff
Nacho Mas <nacho.mas@datadoghq.com>
2025-06-24 14:42:56 +0200

    Adds logging to troubleshoot installation errors

--- install.sh
+++ install.sh
@@ -1,38 +1,5 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+LOGFILE="/home/bits/install.log"
 
-# Update repositories
-sudo apt update && sudo apt --yes upgrade
-
-# Install Homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-## Add Homebrew to PATH for the remainder of the script
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
-## Add Datadog tap
-brew tap datadog/tap git@github.com:DataDog/homebrew-tap.git
-brew update
-## Install fx
-brew install fx
-## Install OrgStore CLI
-brew install orgstore
-
-# Install fzf
-## Remove existing apt installation as it's outdated
-sudo apt remove -y fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --completion --key-bindings --no-update-rc
-
-# Install tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Install pyenv
-curl -fsSL https://pyenv.run | bash
-
-# Handle dotfiles
-## Install stow
-sudo apt install stow
-## Delete existing dotfiles to avoid conflicts
-rm ~/.tmux.conf ~/.zshrc
-## Symlink dotfiles
-stow --dir ~/dotfiles/dotfiles --target ~ tmux zsh
+bash install_0.sh 2>&1 | tee -a "$LOGFILE"
---
+++ install_0.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+function log() {
+    echo "=== [$(date)] $1"
+}
+
+log "Starting installation"
+log "Running as: $(whoami)"
+log "Current workdir: $(pwd)"
+
+# Update repositories
+sudo apt update && sudo apt --yes upgrade
+log "Updated repositories and upgraded packages"
+
+# Install Homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+log "Installed Homebrew"
+## Add Homebrew to PATH for the remainder of the script
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+## Add Datadog tap
+brew tap datadog/tap git@github.com:DataDog/homebrew-tap.git
+brew update
+## Install fx
+brew install fx
+## Install OrgStore CLI
+brew install orgstore
+
+# Install fzf
+## Remove existing apt installation as it's outdated
+sudo apt remove -y fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --completion --key-bindings --no-update-rc
+
+# Install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install pyenv
+curl -fsSL https://pyenv.run | bash
+
+# Handle dotfiles
+## Install stow
+sudo apt install stow
+## Delete existing dotfiles to avoid conflicts
+rm ~/.tmux.conf ~/.zshrc
+## Symlink dotfiles
+stow --dir ~/dotfiles/dotfiles --target ~ tmux zsh
